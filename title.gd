b176f4a7657aa0dc9034a9f340d5f8bc2b2de31a
SalaCodes <salahdinbakouri1@gmail.com>
2025-10-09 14:50:01 +0200

    Auto-backup: 2025-10-09 14:50:01

--- title.gd
+++ title.gd
@@ -44,4 +44,5 @@ func _on_play_pressed() -> void:
 
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	if !quitting:
+		get_tree().change_scene_to_file("res://scenes/settings.tscn")
