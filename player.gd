4c213ee470caa5952b8398b0aad770ec2950f5f5
SalaCodes <salahdinbakouri1@gmail.com>
2025-10-12 10:40:01 +0200

    Auto-backup: 2025-10-12 10:40:01

--- player.gd
+++ player.gd
@@ -21,6 +21,7 @@ class_name Player
 @onready var death_particles: GPUParticles2D = $death_particles
 @onready var hplabelthing: Label = $hplabelthing
 @onready var heal_animation: AnimationPlayer = $heal
+@onready var death: AnimatedSprite2D = %death
 
 
 #animated players onready variables skins
@@ -156,7 +157,7 @@ func _process(delta: float) -> void:
 			candash = true
 	
 	character_skin(Characters.chosen_character["skin"])
-	
+	death.play(str(health_animation))
 	
 	if health == 1:
 		health_animation = 5
