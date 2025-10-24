67627037cb6f35a44b6c55b8faf28afde591820f
SalaCodes <salahdinbakouri1@gmail.com>
2025-10-24 14:10:01 +0200

    Auto-backup: 2025-10-24 14:10:01

--- autoloads/cursor.gd
+++ autoloads/cursor.gd
@@ -3,7 +3,7 @@ extends AnimatedSprite2D
 
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	pass
 
 func _process(delta: float) -> void:
 	if get_tree().current_scene and get_tree().current_scene.is_in_group("game"):
